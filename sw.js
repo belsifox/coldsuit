dea2d04c83dd2ed3a42e772281bdd07b2866ce6a
belsifox <belsi@mailfence.com>
2026-09-18 23:59:44 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'coldsuit-v7';
+const CACHE = 'coldsuit-v8';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
